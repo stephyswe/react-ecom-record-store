be5316f2ecef9a405107d9cd70afc839ed1e27a2
Stephanie <stephaniiehallberg@gmail.com>
2019-08-02 13:02:19 +0200

    Product style and animation

--- src/context.js
+++ src/context.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-
+import { storeProducts, detailProduct } from './data';
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    detailProduct
+  };
+  handleDetail = () => console.log('hello from detail');
+  addToCart = () => console.log('hello from add to cart');
+
   render() {
     return (
-      <ProductContext.Provider value="hello from context">
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
